6b0a4059c268f24352daef7e5e7d41dc80230108
Andrew Scholly <SATmaster728@gmail.com>
2020-02-06 14:53:43 -0500

    Fixed some small errors, noticed area of problem

--- rooms.py
+++ rooms.py
@@ -1,26 +1,38 @@
 import test_value
 import random_functions
-
+from textwrap import dedent
 class Room(object):
     def enter(self):
         print("I have a plan")
         exit(1)
+
+#Currently, I have been unable to find a way to properly use a function with
+#the y variable, despite the fact they are repeated multiple times and
+#are quite messy with the code, same with the check for all of the parts being
+#collected
 
 
 
 class Room1(Room):
     def enter(self):
         x = random_functions.random_number()
-        print("You enter the corridor, looking around for the map to the pieces, when a darkness being jumps out at you!")
-        print("Without any pieces yet, you need to defeat the being somehow without touching him.")
-        print("There are a few rocks, a scroll, and a mysterious blaster with warning signs on it nearby")
+        #used for the randomization, for whether or not s piece is collected
+        print(dedent("""You enter the corridor, looking around for the map to
+the pieces, when a darkness being jumps out at you! Without any pieces yet, you
+need to defeat the being somehow without touching him. There are a few rocks, a
+scroll, and a mysterious blaster with warning signs on it nearby"""))
         fight = input(" ")
+        #The following code is used for whatever the user puts in to decide the output
         if fight == "fire blaster":
             print("You destroyed the enemy, and moved on")
             if x == 1:
+                #uses the x as a variable so there is a chance of collecting or not collecting stuff
                 print("You find the map to the pieces and get the heck out of there")
                 test_value.a = 1
+                #get around the problem of vsriables defined in this room from reverting back to 0
+                #if defined here in the eiter area
                 y = random_functions.random_rooms()
+                #a different random function, so it is not confuse
                 if y == 2:
                     return 'room2'
                 elif y == 3:
@@ -41,6 +53,7 @@ class Room1(Room):
             print("You died")
             return 'Death'
         else:
+            #In case if the user puts something in wrong
             print("That is not a valid response. Try throwing or firing something")
             return 'room1'
 
